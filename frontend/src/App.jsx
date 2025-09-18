02efe74b686ad8da3d3e1896034fea4ba2314558
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2025-09-18 08:55:31 +0200

    frontend starter

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+    const [] = useState(null);
+
+    return (
+        <div style={{ padding: "2rem" }}>
+            <h1>Quotes API (React + Vite)</h1>
+
+        </div>
+    );
+}
+
+export default App;
