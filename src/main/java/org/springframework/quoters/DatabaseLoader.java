9120e9e6ff49b5b8d1f079172f148d1b94640e96
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2025-09-18 08:34:32 +0200

    init

---
+++ src/main/java/org/springframework/quoters/DatabaseLoader.java
@@ -0,0 +1,17 @@
+package org.springframework.quoters;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class DatabaseLoader {
+
+//    @Bean
+//    CommandLineRunner init(QuoteRepository repository) {
+//
+//        return args -> {
+//            repository.save(new Quote("Working with Spring Boot is like pair-programming with the Spring developers."));
+//        };
+//    }
+}
---
+++ src/main/java/org/springframework/quoters/QuoteController.java
@@ -0,0 +1,5 @@
+package org.springframework.quoters;
+
+public class QuoteController {
+
+}
